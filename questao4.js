d0337986185ce1762038e302c12bf16dcc3dd9f2
Israel Oliveira <35226183+raedman90@users.noreply.github.com>
2021-03-31 11:08:22 -0300

    Add files via upload

---
+++ questao4.js
@@ -0,0 +1,13 @@
+function calculateCircumference(radius){
+    return Math.PI * (radius * radius);
+}
+
+console.log(Math.PI);
+
+console.log(calculateCircumference(10));
+
+console.log(Math.round(calculateCircumference(10)));
+
+console.log(Math.ceil(calculateCircumference(10)));
+
+console.log(Math.floor(calculateCircumference(10)));
